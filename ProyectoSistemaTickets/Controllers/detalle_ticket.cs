24a0db5f4a668c70b9ab4bdbd844cd8eec69e9ab
NatalySolis <natycrotez1@outlook.es>
2024-05-22 19:37:30 -0600

    Vista de detalle ticket, para admin y empleado

---
+++ ProyectoSistemaTickets/Controllers/detalle_ticket.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoSistemaTickets.Controllers
+{
+    public class detalle_ticket : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
