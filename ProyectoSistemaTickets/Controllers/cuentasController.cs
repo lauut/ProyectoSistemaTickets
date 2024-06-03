a315cf2f834accb9e8307d63f474ff1e3ab1a815
NatalySolis <natycrotez1@outlook.es>
2024-06-02 18:07:26 -0600

    login :(

--- ProyectoSistemaTickets/Controllers/cuentasController.cs
+++ ProyectoSistemaTickets/Controllers/cuentasController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoSistemaTickets.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ProyectoSistemaTickets.Controllers
 {
     public class cuentasController : Controller
     {
+        private readonly CompanyContext _context;
+
+        public cuentasController(CompanyContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult ValidarUsuario(cuentas cuenta)
+        {
+
+            cuentas? usuario = (from user in _context.cuentas
+                                where user.usuario == cuenta.usuario
+                                && user.contra == cuenta.contra
+                                select user).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                ViewBag.Mensaje = "Usuario o contraseña incorrectos :(";
+                return View("Index");
+
+                String datosUsuario = JsonSerializer.Serialize(usuario);
+                HttpContext.Session.SetString("user", datosUsuario);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
     }
 }
+
+
+
