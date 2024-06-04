d7dd90d141d9d9323f2debe6aebbf18052396b4d
NatalySolis <natycrotez1@outlook.es>
2024-06-03 18:02:43 -0600

    combobox de detalle de ticket y empleado y admi

---
+++ ProyectoSistemaTickets/Controllers/detalle_admiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoSistemaTickets.Models;
+
+namespace ProyectoSistemaTickets.Controllers
+{
+    public class detalle_admiController : Controller
+    {
+        private readonly CompanyContext _context;
+
+        public detalle_admiController(CompanyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var listaDePrioridad = (from p in _context.prioridad
+                                    select p).ToList();
+            ViewData["listadoDePrioridad"] = new SelectList(listaDePrioridad, "idpriori", "nombre");
+
+            var listaDeEstados = (from es in _context.estados
+                                  select es).ToList();
+            ViewData["listadoDeEstados"] = new SelectList(listaDeEstados, "idestado", "nombre");
+
+            return View();
+        }
+    }
+}
---
+++ ProyectoSistemaTickets/Controllers/detalle_ticketController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoSistemaTickets.Models;
+
+namespace ProyectoSistemaTickets.Controllers
+{
+    public class detalle_ticketController : Controller
+    {
+        private readonly CompanyContext _context;
+
+        public detalle_ticketController(CompanyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var listaDeEmpleados = (from e in _context.empleados
+                                    select e).ToList();
+            ViewData["listadoDeEmpleados"] = new SelectList(listaDeEmpleados, "idempleado", "nombre");
+
+
+            var listaDePrioridad = (from p in _context.prioridad
+                                    select p).ToList();
+            ViewData["listadoDePrioridad"] = new SelectList(listaDePrioridad, "idpriori", "nombre");
+
+            var listaDeEstados = (from es in _context.estados
+                                    select es).ToList();
+            ViewData["listadoDeEstados"] = new SelectList(listaDeEstados, "idestado", "nombre");
+
+            return View();
+
+        }
+
+       
+    }
+}
